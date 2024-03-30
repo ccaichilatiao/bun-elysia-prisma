a9060f7745ddf4bfdeeba2440899cd82854b3f28
ccaichilatiao <ccaichilatiao111@gmail.com>
2024-03-30 18:44:01 +0800

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,21 @@
 import { Elysia } from "elysia";
+import { swagger } from "@elysiajs/swagger";
 
-const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+import postsRoutes from "./routes/posts";
+
+const app = new Elysia();
+
+class Logger {
+  log(value: string) {
+    console.log(value);
+  }
+}
+
+app
+  .decorate("logger", new Logger())
+  .use(swagger())
+  .group("/api", (app) => app.use(postsRoutes))
+  .listen(process.env.PORT || 8000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
